59d73c6e560e72ea87555362fc28f9ed03add4db
Fedor Korotkov <fkorotkov@openai.com>
2026-07-09 18:35:02 -0400

    Use smaller Vetu image for Linux tests

--- internal/imageconstant/imageconstant.go
+++ internal/imageconstant/imageconstant.go
@@ -2,5 +2,5 @@ package imageconstant
 
 const (
 	DefaultMacosImage = "ghcr.io/cirruslabs/macos-tahoe-base:latest"
-	DefaultLinuxImage = "ghcr.io/cirruslabs/ubuntu-runner-amd64:latest"
+	DefaultLinuxImage = "ghcr.io/cirruslabs/ubuntu:latest"
 )
